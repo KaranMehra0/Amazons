c1ae66a69db344e0a5314a4b48b6a45c739a6a9b
KaranMehra0 <Karanmehra22333@gmail.com>
2025-07-22 13:14:31 +0530

    init

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href='/'>Amazons</a>
+      </header>
+      <main>List Project</main>
+    </div>
+  );
+}
+
+export default App;
